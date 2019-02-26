23ac73999da7c22e9d4ac1f108f4d1b2589459c6
Daniel Lemire <lemire@gmail.com>
2019-02-26 11:30:54 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, Extension
 module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
-        version = '1.0',
+        version = '1.1',
         description = 'Fast random number generation in Python',
         ext_modules = [module1])
