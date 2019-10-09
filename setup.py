0d28fd80be4746b9b4ff79ad5251637ed37ba6ce
Daniel Lemire <lemire@gmail.com>
2019-10-09 12:22:12 -0400

    Another minor release

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, Extension
 module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
-        version = '1.2.1',
+        version = '1.2.2',
         description = 'Fast random number generation in Python',
         ext_modules = [module1])
