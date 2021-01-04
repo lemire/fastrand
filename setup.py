308e25d1ae5750a39caada5dc7b24ca0e00f57a7
Daniel Lemire <lemire@gmail.com>
2021-01-04 10:03:34 -0500

    Changing version.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, Extension
 module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
-        version = '1.2.4',
+        version = '1.3.0',
         description = 'Fast random number generation in Python',
         ext_modules = [module1])
