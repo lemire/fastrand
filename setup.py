57322fa6418ca4406c868aaf37a6ff6d96af7212
Daniel Lemire <daniel@lemire.me>
2023-02-03 10:10:44 -0500

    Preparing new release

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, Extension
 module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
-        version = '1.3.0',
+        version = '1.4.0',
         description = 'Fast random number generation in Python',
         ext_modules = [module1])
