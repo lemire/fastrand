87cd8b8b9772b274062a4fcfd92fe3212ddf5e77
Daniel Lemire <daniel@lemire.me>
2023-05-02 14:31:10 -0400

    Patch release

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
         author="Daniel Lemire",
-        version = '1.7.0',
+        version = '1.7.1',
         author_email='daniel@lemire.me',
-        long_description=open('README.md', 'r').read(),
-        long_description_content_type='text/markdown',
+        long_description = open('README.md', 'r', encoding="utf-8").read(),
+        long_description_content_type = 'text/markdown',
         description = 'Fast random number generation in Python',
         ext_modules = [module1])
