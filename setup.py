bf1c86750d1a3237b2a5e46865a0d62bf22aa4b4
Daniel Lemire <daniel@lemire.me>
2025-05-13 13:26:31 -0400

    another attempt.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ module1 = Extension('fastrand', sources = ['fastrandmodule.c'])
 
 setup (name = 'fastrand',
         author="Daniel Lemire",
-        version = '3.0.4',
+        version = '3.0.5',
         author_email='daniel@lemire.me',
         long_description = open('README.md', 'r', encoding="utf-8").read(),
         long_description_content_type = 'text/markdown',
